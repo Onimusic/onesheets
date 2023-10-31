5ca822dcaac925c9916f277de9da8341438bb24e
oricardos <ricardo.rs95@gmail.com>
2023-10-31 15:26:45 -0300

    add makeId

---
+++ src/utils/makeId.tsx
@@ -0,0 +1,17 @@
+/**
+ * Gerador de strings aleatório
+ * @param {number} length tamanho da string
+ * @returns {string}
+ */
+function makeId(length:number) {
+    var result = "";
+    var characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+        );
+    }
+    return result;
+}
